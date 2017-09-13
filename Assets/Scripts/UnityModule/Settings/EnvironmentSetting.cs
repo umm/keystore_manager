cf8f9619a7f7f8a81854f593e656544f1749a543
Tetsuya Mori <monry84@gmail.com>
2017-09-13 12:16:34 +0900

    :seedling: Initial commit

---
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityModule.Settings {
+
+    // ReSharper disable once PartialTypeWithSinglePart
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class EnvironmentSetting : Setting<EnvironmentSetting> {
+
+        /// <summary>
+        /// Keystore の情報を管理する構造体
+        /// </summary>
+        [Serializable]
+        public struct Keystore {
+
+            public string Path;
+
+            public string Passphrase;
+
+            public string AliasName;
+
+            public string AliasPassphrase;
+
+        }
+
+        /// <summary>
+        /// 開発向けのキーストアの実体
+        /// </summary>
+        [SerializeField]
+        private Keystore keystoreDevelopment;
+
+        /// <summary>
+        /// 開発向けのキーストア
+        /// </summary>
+        public Keystore KeystoreDevelopment {
+            get {
+                return this.keystoreDevelopment;
+            }
+            set {
+                this.keystoreDevelopment = value;
+            }
+        }
+
+        /// <summary>
+        /// 本番向けのキーストアの実体
+        /// </summary>
+        [SerializeField]
+        private Keystore keystoreProduction;
+
+        /// <summary>
+        /// 本番向けのキーストア
+        /// </summary>
+        public Keystore KeystoreProduction {
+            get {
+                return this.keystoreProduction;
+            }
+            set {
+                this.keystoreProduction = value;
+            }
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# What?
+
+* Android の Keystore を管理します。
+
+# Why?
+
+* ビルド時に環境依存な設定としての Keystore 情報を必要とするため実装しました。
+
+# Install
+
+```shell
+$ npm install github:umm-projects/keystore_manager
+```
+
+# Usage
+
+1. `Assets` &gt; `Create` &gt; `Setting` &gt; `EnvironmentSetting` から環境依存設定用ファイルを作成します
+1. `Keystore` の項目を設定します
+1. ビルド時に自動的に反映されます
+
+# License
+
+Copyright (c) 2017 Tetsuya Mori
+
+Released under the MIT license, see [LICENSE.txt](LICENSE.txt)
+
